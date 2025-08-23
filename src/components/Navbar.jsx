6f89308c7f28778a56f2a671b4c443f41e6b6111
KSAnushree <anushreetutu2006@gmai.com>
2025-08-23 23:42:52 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,24 +5,31 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
 
   const handleNav = () => {
-    setNav = !nav;
+    setNav(!nav);
   };
 
   return (
     <div className="flex justify-between items-center h-24 max-w-full mx-auto px-4 text-white">
-      <h1 className="w-full text-3xl font-bold text-green-600">REACT.</h1>
-      <ul className="flex hidden">
+      <h1 className="w-full text-4xl font-bold text-green-600 m-6">REACT.</h1>
+      <ul className="hidden md:flex">
         <li className="p-5">Home</li>
         <li className="p-5">Company</li>
         <li className="p-5">Resource</li>
         <li className="p-5">About</li>
         <li className="p-5">Contact</li>
       </ul>
-      <div>
-        <AiOutlineMenu size={20} />
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-      <div className="fixed left-0 top-0 w-[15%] h-full border-r border-r-gray-900 bg-black">
-        <h1 className="w-full text-3xl font-bold text-green-600 m-4">REACT.</h1>
+
+      <div
+        className={
+          !nav
+            ? "fixed left-0 top-0 w-[20%] h-full border-r border-r-gray-900 bg-black ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }
+      >
+        <h1 className="w-full text-4xl font-bold text-green-600 m-9">REACT.</h1>
         <ul className="uppercase p-4">
           <li className="p-5 border-b border-gray-600">Home</li>
           <li className="p-5 border-b border-gray-600">Company</li>
